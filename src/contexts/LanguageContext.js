349b234be92642e2f9aec644b56dd4a46e4ed53c
Arif <64827038+arifakbar@users.noreply.github.com>
2021-03-20 11:35:00 +0530

    Update LanguageContext.js

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -18,25 +18,3 @@ export class LanguageStore extends React.Component {
 
 export default Context;
 
-// import React from 'react';
-
-// const Context = React.createContext('english');
-
-// export class LanguageStore extends React.Component {
-//   state = { language: 'english' };
-
-//   setLanguage = language => {
-//     this.setState({ language });
-//   };
-
-//   render() {
-//     return (
-//       <Context.Provider
-//         value={{ ...this.state, setLanguage: this.setLanguage }}
-//       >
-//         {this.props.children}
-//       </Context.Provider>
-//     );
-//   }
-// }
-// export default Context;
